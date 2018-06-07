bb6581c054df1709e3677d45fd4c1b884b8ee3c0
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-06-07 22:23:35 +0100

    cleanup

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,6 @@
-import express from 'express'
 import TestPortletServer from "./TestPortletServlet"
 
 const port = process.env.PORT || 8080
-const app = express()
 const server = new TestPortletServer('../../dist/portlet.js');
 process.on('uncaughtException', (err) => {
   console.error(err)
